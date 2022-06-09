e094d76c825bc4ec553c9b27c6ce51ec63fbeb6d
huutrungu <huutrungg02@gmail.com>
2022-06-09 16:21:10 +0700

    cs task 2

--- src/casestudy/angular-furama/src/app/app-routing.module.ts
+++ src/casestudy/angular-furama/src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {FacilitiesListComponent} from './facilities/facilities-list/facilities-list.component';
+import {CustomerListComponent} from './customers/customer-list/customer-list.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {ContractListComponent} from './contracts/contract-list/contract-list.component';
 
 
-const routes: Routes = [];
+// const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'facilityList', component: FacilitiesListComponent},
+  {path: 'customerList', component: CustomerListComponent},
+  {path: 'contractList', component: ContractListComponent},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),
+    BrowserModule,
+    FormsModule],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
