fa80b43fe8df9ae206a04212b0a4ef388c0dc13b
huutrungu <huutrungg02@gmail.com>
2022-06-10 17:15:28 +0700

    ss5

--- src/casestudy/angular-furama/src/app/app-routing.module.ts
+++ src/casestudy/angular-furama/src/app/app-routing.module.ts
@@ -5,14 +5,24 @@ import {CustomerListComponent} from './customers/customer-list/customer-list.com
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import {ContractListComponent} from './contracts/contract-list/contract-list.component';
+import {CustomerEditComponent} from './customers/customer-edit/customer-edit.component';
+import {CustomerCreateComponent} from './customers/customer-create/customer-create.component';
+import {FacilitieEditComponent} from './facilities/facilitie-edit/facilitie-edit.component';
+import {FacilitiesCreateComponent} from './facilities/facilities-create/facilities-create.component';
+import {ContractCreateComponent} from './contracts/contract-create/contract-create.component';
 
 
 // const routes: Routes = [];
 
 const routes: Routes = [
   {path: 'facilityList', component: FacilitiesListComponent},
+  {path: 'facilityEdit', component: FacilitieEditComponent},
+  {path: 'facilityCreate', component: FacilitiesCreateComponent},
   {path: 'customerList', component: CustomerListComponent},
   {path: 'contractList', component: ContractListComponent},
+  {path: 'contractCreate', component: ContractCreateComponent},
+  {path: 'customerEdit', component: CustomerEditComponent},
+  {path: 'customerCreate', component: CustomerCreateComponent},
 ];
 
 @NgModule({
