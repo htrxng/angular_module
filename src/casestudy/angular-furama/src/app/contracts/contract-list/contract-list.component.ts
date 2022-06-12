0baeb5b920756bfd08cb6d188e696d0963addfb0
huutrungu <huutrungg02@gmail.com>
2022-06-12 21:57:05 +0700

    task 3

--- src/casestudy/angular-furama/src/app/contracts/contract-list/contract-list.component.ts
+++ src/casestudy/angular-furama/src/app/contracts/contract-list/contract-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {contractList} from '../../data/contractList';
 
 @Component({
   selector: 'app-contract-list',
@@ -6,8 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contract-list.component.css']
 })
 export class ContractListComponent implements OnInit {
+  contractList = contractList;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
